56fe2d36194bdbfb9fa634dd76a861c202d5f5d0
Geek <y_t0507@yahoo.co.jp>
2018-11-04 19:20:10 +0900

    済：入力部、確認部での入力内容表示
    未：確認部の送信ボタン実装、データベースへの送信

--- test.php
+++ test.php
@@ -1,3 +1,53 @@
 <?php
-echo '<p>Hello World</p>';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $Aform = array_fill(0, 4, '');
+    $Qval = '';
+    $Aval = array_fill(0, 4, '');
+    $err = false;
+    $Aformsearch = array_search(false, $_POST['Aform']);
+    if(empty($_POST['Qform']) === false && $Aformsearch === false) {
+        $Qval = $_POST['Qform'];
+        $Aval = $_POST['Aform'];
+        $err = true;
+    } else {
+        echo 'ERROR! 全ての入力フォームを入力してください。';
+    }
+}
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Input Form</title>
+</head>
+<body>
+  <form action="" method="POST">
+    <textarea cols="60" rows="10" name="Qform" placeholder="問題文を入力"> </textarea>
+    <br>
+
+    <input type="text" name="Aform[0]" placeholder="回答1を入力"
+    value="<?php echo htmlspecialchars($Aform[0], ENT_QUOTES, 'UTF-8'); ?>">
+    <input type="text" name="Aform[1]" placeholder="回答2を入力"
+    value="<?php echo htmlspecialchars($Aform[1], ENT_QUOTES, 'UTF-8'); ?>">
+    <input type="text" name="Aform[2]" placeholder="回答3を入力"
+    value="<?php echo htmlspecialchars($Aform[2], ENT_QUOTES, 'UTF-8'); ?>">
+    <input type="text" name="Aform[3]" placeholder="回答4を入力"
+    value="<?php echo htmlspecialchars($Aform[3], ENT_QUOTES, 'UTF-8'); ?>">
+
+    <input type="submit" value="Send!">
+    <?php
+       session_start();
+       if ($err) {
+           $_SESSION['Qval'] = htmlspecialchars($Qval, ENT_QUOTES, "UTF-8");
+           foreach($Aval as $key => $value) {
+               $_SESSION['Aval'][$key] = $value;
+           }
+           http_response_code(301);
+           header( "Location:./Confirm.php");
+           exit;
+       }
+    ?>
+  </form>
+</body>
+</html>
